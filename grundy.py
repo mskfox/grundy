5038917c3f3e6f886dd45eed57061e6e67405797
Matthew S K Fox <matthewskfox@gmail.com>
2025-01-12 15:59:38 +0100

    Add automatic cleaning of __pycache__ after execution

--- grundy.py
+++ grundy.py
@@ -1,4 +1,16 @@
 import runpy
+import os
+import shutil
+
+
+def clean_pycache(start_dir='.'):
+    for root, dirs, files in os.walk(start_dir, topdown=False):
+        if '__pycache__' in dirs:
+            pycache_dir = os.path.join(root, '__pycache__')
+            print(f"Removing {pycache_dir}")
+            shutil.rmtree(pycache_dir)
+
 
 if __name__ == "__main__":
     runpy.run_module('grundy', run_name='__main__')
+    clean_pycache()
